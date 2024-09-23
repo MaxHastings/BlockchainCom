ab7131224f67159f0f84b7bba0de26743b8e3148
Max Hastings <max.jacob.hastings@gmail.com>
2024-09-23 14:48:45 -0500

    remove unused code

--- app/src/main/java/com/example/blockchaincom/data/remote/releases/ReleasesApi.kt
+++ app/src/main/java/com/example/blockchaincom/data/remote/releases/ReleasesApi.kt
@@ -1,9 +1,5 @@
 package com.example.blockchaincom.data.remote.releases
 
-import retrofit2.http.GET
-import retrofit2.http.Path
-
 interface ReleasesApi {
-    @GET("artists/{artist_id}/releases")
-    suspend fun getArtistReleases(@Path("artist_id") artistId: Int): PaginationResponse
+    suspend fun getArtistReleases(artistId: Int): PaginationResponse
 }
