9dbfb06cea8eef7eee8ee7ff9d01a41c42512cc2
Max Hastings <max.jacob.hastings@gmail.com>
2024-09-23 12:22:28 -0500

    Add documentation for DiscogsApiImpl

--- app/src/main/java/com/example/blockchaincom/DiscogsApiImpl.kt
+++ app/src/main/java/com/example/blockchaincom/DiscogsApiImpl.kt
@@ -8,11 +8,25 @@ import okhttp3.Request
 import java.io.IOException
 import javax.inject.Inject
 
+/**
+ * Implementation of the [DiscogsApi] interface that uses OkHttp to make network requests
+ * and Gson to parse JSON responses.
+ *
+ * @param httpClient The OkHttp client used for making network requests.
+ * @param gson The Gson instance used for parsing JSON responses.
+ */
 class DiscogsApiImpl @Inject constructor(
     private val httpClient: OkHttpClient,
     private val gson: Gson
 ) : DiscogsApi {
 
+    /**
+     * Retrieves a list of releases for a given artist ID from the Discogs API.
+     *
+     * @param artistId The ID of the artist.
+     * @return A [PaginationResponse] object containing the list of releases and pagination information.
+     * @throws IOException If there is an error making the network request or parsing the response.
+     */
     override suspend fun getArtistReleases(artistId: Int): PaginationResponse {
         val request = Request.Builder()
             .url("https://api.discogs.com/artists/$artistId/releases")
